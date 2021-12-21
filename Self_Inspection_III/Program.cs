d5b8b33d802f7c0476bada65b8a326ef860a98b0
GaryH209395 <gary.huang@chroma.com.tw>
2021-12-21 10:39:18 +0800

    202112211039

--- Self_Inspection_III/Program.cs
+++ Self_Inspection_III/Program.cs
@@ -14,7 +14,6 @@ namespace Self_Inspection_III
         [STAThread]
         static void Main()
         {
-            //
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
